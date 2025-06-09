69cff63ccae84bd758efa5cda910a2595b6fbb4f
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-06-09 20:16:17 +0900

    🎨 RefresshTokenをRedisに格納する様にしたのでContext修正

--- apps/api/Infrastructure/Persistence/ApplicationDbContext.cs
+++ apps/api/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -7,7 +7,6 @@ namespace Api.Infrastructure.Persistence
   public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
   {
     public DbSet<UserEntity> Users { get; set; }
-    public DbSet<RefreshTokenEntity> RefreshTokens { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -16,12 +15,12 @@ namespace Api.Infrastructure.Persistence
       modelBuilder.Entity<UserEntity>(entity =>
       {
         entity.HasKey(e => e.Id);
-        entity.Property(e => e.Id).ValueGeneratedOnAdd(); // long型の主キーはDBで自動生成
+        entity.Property(e => e.Id).ValueGeneratedOnAdd();
 
         entity.Property(e => e.Uuid)
               .HasConversion(
                   uuid => uuid.Value,
-                  value => new Uuid(value)
+                  dbValue => new Uuid(dbValue)
               )
               .IsRequired();
         entity.HasIndex(e => e.Uuid).IsUnique(); // Uuidはユニークであるべき
@@ -40,25 +39,8 @@ namespace Api.Infrastructure.Persistence
                .HasColumnName("Email")
                .HasMaxLength(320)
                .IsRequired();
-          // Email Value Object の Value プロパティに対してユニーク制約を設定
           emailBuilder.HasIndex(vo => vo.Value).IsUnique().HasDatabaseName("IX_User_Email");
         });
-
-      });
-
-      modelBuilder.Entity<RefreshTokenEntity>(entity =>
-      {
-        entity.HasKey(e => e.Id);
-        entity.Property(e => e.Id).HasConversion(id => id.Value, value => new Uuid(value));
-
-        entity.Property(e => e.UserId)
-              .HasConversion(
-                  uuid => uuid.Value,
-                  value => new Uuid(value)
-              )
-              .IsRequired();
-
-        entity.HasOne<UserEntity>().WithMany().HasForeignKey(rt => rt.UserId).HasPrincipalKey(u => u.Uuid);
       });
     }
   }
